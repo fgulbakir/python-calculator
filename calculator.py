ae29fe9a2215ddd7ea1b48d4925ec48be3d89849
fgulbakir <fatmagulbakir61@gmail.com>
2020-04-14 12:08:27 +0300

    python simple calculator

---
+++ calculator.py
@@ -0,0 +1,13 @@
+""" Simple Calculator  """
+
+print("Enter first number :")
+firstNumber = int(input()) 
+print("Enter second number :")
+secondnumber = int(input())
+
+print(str(firstNumber) + " + " + str(secondnumber)+ " = " + str(firstNumber + secondnumber))
+print(str(firstNumber) + " - " + str(secondnumber)+ " = " + str(firstNumber - secondnumber))
+print(str(firstNumber) + " * " + str(secondnumber)+ " = " + str(firstNumber * secondnumber))
+print(str(firstNumber) + " / " + str(secondnumber)+ " = " + str(firstNumber / secondnumber))
+
+
